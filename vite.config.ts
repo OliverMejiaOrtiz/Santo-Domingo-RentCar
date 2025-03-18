2ec315fdab8f2e588a20d0692e698ff99deef546
Oliver Mejía <mejiaortizoliver92100@gmail.com>
2025-03-18 19:29:30 -0400

    Fix: Solucionar problemas con imágenes en Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +19,12 @@ export default defineConfig({
           vendor: ['react', 'react-dom'],
         },
       },
+    },
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': path.resolve(process.cwd(), 'src'),
     },
   },
 });
